76120400ec5bd3c7865a243233359dbf89a45e02
Gogyungho <whohow10@gmail.com>
2021-09-15 03:26:15 +0900

    connect

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import { increase, decrease, setDiff } from '../modules/counter';
 
-function CounterContainer() {
-    const { number, diff } = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff,
-    }))
-    const dispatch = useDispatch();
-    const onIncrease = () => dispatch(increase());
-    const onDecrease = () => dispatch(decrease());
-    const onSetDiff = diff => dispatch(setDiff(diff))
+function CounterContainer({
+    number,
+    diff,
+    onIncrease,
+    onDecrease,
+    onSetDiff,
+}) {
     return(
         <Counter
             number={number}
@@ -23,4 +21,14 @@ function CounterContainer() {
     )
 };
 
-export default CounterContainer;
+const mapStateToProps = (state) => ({
+     number: state.counter.number,
+     diff: state.counter.diff,
+});
+const mapDispatchToProps = dispatch  => ({
+    onIncrease: () => dispatch(increase()),
+    onDecrease: () => dispatch(decrease()),
+    onSetDiff: (diff) => dispatch(setDiff(diff)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,14 +1,15 @@
-import React, { useCallback } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import React from 'react';
+import {connect} from 'react-redux';
 import Todos from '../components/Todos';
 import {addTodo, toggleTodo} from '../modules/totos';
 
-function TodosContainer() {
-    const todos = useSelector(state => state.todos);
-    const dispatch = useDispatch();
-
-    const onCreate = useCallback(text => dispatch(addTodo(text)), [dispatch]);
-    const onToggle = useCallback(id => dispatch(toggleTodo(id)), [dispatch]);
+function TodosContainer({
+    todos,
+    onCreate,
+    onToggle,
+}) {
+    // const onCreate = useCallback(text => dispatch(addTodo(text)), [dispatch]);
+    // const onToggle = useCallback(id => dispatch(toggleTodo(id)), [dispatch]);
 
     return <Todos
         todos={todos}
@@ -17,4 +18,11 @@ function TodosContainer() {
     />
 }
 
-export default TodosContainer;
+const mapStateToProps = state => ({todos: state.todos});
+const mapDispatchToProps = dispatch => ({
+    onCreate: (text) => dispatch(addTodo(text)),
+    onToggle: (id) => dispatch(toggleTodo(id)),
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
